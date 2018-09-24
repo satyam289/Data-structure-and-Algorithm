40a27736efc73baf691aef6d6f1aa9f304136ab0
Satyam <bitsat28@gmail.com>
2018-09-24 22:44:13 +0530

    --MobilePhoneCombination --NQueen --Distinct Subsequence

---
+++ src/Backtracking/NQueen.java
@@ -0,0 +1,56 @@
+package Backtracking;
+
+import java.util.Arrays;
+
+public class NQueen {
+
+	public static void main(String[] args) {
+	  solveNQueen();	
+	}
+
+	public static void solveNQueen(){
+		int board [][]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+		dosolveNQueen(board,0);
+		for(int i=0;i<board.length;i++){
+			for(int j=0;j<board[i].length;j++){
+				System.out.print(board[i][j]+"  ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static boolean dosolveNQueen(int[][] board, int col){
+		
+		if(col>=board.length)
+			return true;
+		for(int i=0;i<board.length;i++){
+			  
+			if(safe(board,i,col)){
+			   board[i][col]=1;
+			    
+			   if(dosolveNQueen(board,col+1))
+				   return true;
+			   board[i][col]=0;
+			
+		     }
+	    }
+		return false;	
+	}
+	
+	public static boolean safe(int [][] board, int row, int col){
+		
+		for(int i=0;i<col;i++){
+			if(board[row][i]==1)
+				return false;
+		}
+		for(int i=row,j=col;i>=0 && j>=0 ;i--,j--){
+			if(board[i][j]==1)
+				return false;
+		}
+		for(int i=row,j=col;i<board.length && j>=0 ;i++,j--){
+		   if(board[i][j]==1)
+			   return false;
+		}
+		return true;
+	}
+}
