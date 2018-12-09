0cf0f889c8566c0e56dcd9637d67a832d363ffcd
satyam <bitsat28@gmail.com>
2018-12-09 21:41:03 +0530

    --Topology Sorting

---
+++ src/graph/TopologySort.java
@@ -0,0 +1,73 @@
+package graph;
+
+
+import java.util.*;
+
+/*
+  For directed Acyclic Graph
+ */
+
+public class TopologySort<T> {
+
+    public void TopologySort(Graph2<T> graph) {
+        Stack<Vertex2<T>> stack = new Stack();         // Deque<Vertex> deque = new ArrayDeque<>();
+        Set<Vertex2<T>> visited = new HashSet();
+        for (Vertex2<T> v : graph.getAllVertex()) {      //covering all the vertex
+            topologicalSortUtil(v, stack, visited);
+        }
+        while(!stack.empty()){         // printing
+            System.out.println(stack.pop().data);
+        }
+    }
+
+    public void topologicalSortUtil(Vertex2<T> vertex, Stack<Vertex2<T>> stack, Set<Vertex2<T>> visited) {
+        visited.add(vertex);
+        for (Vertex2<T> adj : vertex.getAllAdajecent()) {
+            if (visited.contains(adj)) {                     //skip if vertex is already visited
+                continue;
+            }
+            topologicalSortUtil(adj, stack, visited);
+        }
+        stack.add(vertex);                             // add only when all its adjacent vertex is visited
+    }
+
+
+    public class Graph2<T> {
+
+        private List<Edge2<T>> allEdges;
+        private Map<Long, Vertex2<T>> allvertex;
+        boolean isDirected;
+
+        Graph2(boolean isDirected) {
+            allEdges = new ArrayList<>();
+            allvertex = new HashMap<>();
+            this.isDirected = isDirected;
+        }
+
+        public List<Vertex2<T>> getAllVertex() {
+            return (List<Vertex2<T>>) allvertex.values();
+
+        }
+    }
+
+    class Vertex2<T> {
+        private T data;
+        private List<Edge2<T>> edges = new ArrayList<>();
+        private List<Vertex2<T>> adjacentVertex = new ArrayList<>();
+
+        public List<Vertex2<T>> getAllAdajecent() {
+            return adjacentVertex;
+        }
+    }
+
+    class Edge2<T> {
+        private boolean isDirected;
+        private Vertex2<T> vertex1;
+        private Vertex2<T> vertex2;
+        private int weight;
+    }
+}
+
+
+
+
