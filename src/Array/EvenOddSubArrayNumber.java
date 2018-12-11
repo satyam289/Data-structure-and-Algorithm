5814179ee1b84c8af4b688ee7f6fbb3c6989b7dd
satyam <bitsat28@gmail.com>
2018-12-12 01:51:05 +0530

    --NumberEvenOddSubArray(hackerearth)

---
+++ src/Array/EvenOddSubArrayNumber.java
@@ -0,0 +1,47 @@
+package Array;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+https://www.hackerearth.com/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/practice-problems/algorithm/odd-even-subarrays-72ad69db/
+ */
+
+public class EvenOddSubArrayNumber {
+
+    public static void main(String args[]) throws Exception {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        Map<BigInteger, BigInteger> m = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int input = s.nextInt();
+            if (input % 2 != 0) {
+                arr[i] = 1;
+            }
+        }
+        m.put(BigInteger.ZERO, BigInteger.ONE);
+        BigInteger even = BigInteger.ZERO;
+        BigInteger odd = BigInteger.ZERO;
+        BigInteger count = BigInteger.ZERO;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1)
+                odd = odd.add(BigInteger.ONE);
+            else
+                even = even.add(BigInteger.ONE);
+            BigInteger diff = odd.subtract(even);
+            if (m.get(diff) != null) {
+                BigInteger value = m.get(diff);
+                count = count.add(value);
+                m.put(diff, value.add(BigInteger.ONE));
+            } else {
+                m.put(diff, BigInteger.ONE);
+            }
+        }
+        System.out.println("" + count);
+    }
+}
+
