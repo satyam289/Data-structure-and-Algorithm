0216445e62d90e2cbba337daf41517eab1dfb88a
satyam <bitsat28@gmail.com>
2019-01-07 23:03:00 +0530

    - Sliding Window

---
+++ src/Array/MaximumKSubArray.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class MaximumKSubArray {
+
+    public static void main(String[] args) {
+     int [] arr = {11,-8,16,24,-2,3};
+     findMaxSumOfKSize(arr,3);
+    }
+
+    //sliding window
+    private static void findMaxSumOfKSize(int[] arr, int k) {
+
+        int sum = 0, maxsum = 0;
+        int startindex = 0;
+        for (int i = 0; i < k; i++)
+            sum += arr[i];
+        maxsum = sum;
+        for (int i = k; i < arr.length; i++) {
+            sum = sum + arr[i] - arr[i - k];
+            if (sum > maxsum) {
+                maxsum = sum;
+                startindex = i - k;
+            }
+        }
+        System.out.println("The max sum :" + maxsum + " start at index :" + startindex);
+    }
+}
