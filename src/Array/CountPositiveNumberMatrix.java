2d28b8b82ceeceaaf673adc70e4122779a2f7309
satyam <bitsat28@gmail.com>
2019-01-11 13:38:22 +0530

    --added optimised count positive number

--- src/Array/CountPositiveNumberMatrix.java
+++ src/Array/CountPositiveNumberMatrix.java
@@ -6,31 +6,43 @@ public class CountPositiveNumberMatrix {
 	public static void main(String[] args) {
 
 		int[][] m = {{-3, -2, 1}, {-2, 1, 2}, {2, 5, 6}, {3, 7, 9}};    // matrix is sorted both column wise and row wise
-		int count = 0, row = m.length, col = m[0].length;   //assuming all column is same
-		int size = row;
-		int colSize = col;
-		for (int i = 0; i < row; i++) {
+		countPositiveNumberBrute(m);
+		countPositiveOptimized(m);
+	}
 
-			if (m[0][i] > 0) {
-				count += size * (colSize - i);          //when first element in row is  positive , stop and count the remaining item
-				break;
-			}
-			for (int j = 0; j < col; j++) {
-				//System.out.print(m[i][j]+" ");
-				if (m[j][i] <= 0) {                           //skip , when value is less
-					continue;
+	// 0(n2)
+	private static void countPositiveNumberBrute(int[][] m) {
+		int count = 0;
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length;) {
+				if (m[i][j] <= 0) {
+					j++;
+				}else{
+					count += (m[0].length - j);
+					break;
 				}
-				// System.out.println(m[j][i]);
-
-				if (m[j][i] > 0) {
-					count++;
-					col = j;                               //no need of next iteration
-				}
-				count += ((size - 1) - (col));                    //add the remaining item of that row
 			}
-			// System.out.println();
 		}
-		System.out.println("No of Non negative Number in maxtrix : " + count);
+		System.out.println("No of Non negative Number in matrix : " + count);
+	}
+
+
+	//0(n)
+	public static void countPositiveOptimized(int [][] arr){
+
+	    int i = 0;
+	    int j = arr[0].length-1;
+	    int NegativeCount = 0;
+		while (i < arr.length && j>=0 ) {
+			if (arr[i][j] <= 0) {
+				NegativeCount += (arr.length - j - 2);
+				i++;
+			}else{
+	    		j--;
+			}
+	    }
+	    int positiveCount = ((arr.length) * (arr[0].length)) - NegativeCount;
+		System.out.println("No of Non negative Number in matrix : " + positiveCount);
 	}
 
 }
