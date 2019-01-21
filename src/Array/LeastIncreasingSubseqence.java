cadfb2502123e3f80fe20640a3b323e9c7cf6172
satyam <bitsat28@gmail.com>
2019-01-21 22:11:02 +0530

    --LIS

---
+++ src/Array/LeastIncreasingSubseqence.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Arrays;
+
+public class LeastIncreasingSubseqence {
+
+    public static void main(String[] args) {
+        int[] input = {10, 22, 9, 33, 21, 50, 41, 60 };
+        findLIS(input);
+    }
+
+    public static void findLIS(int [] arr){
+
+        int [] temp = new int[arr.length];
+        Arrays.fill(temp, 1);
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i-1; j>=0; j--) {
+
+                if(arr[i] > arr[j])
+                  temp[i] = Math.max(temp[j] +1, temp[i]);
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for(int val: temp)
+            if(val> max)
+                max = val;
+
+        System.out.println(max);
+
+    }
+
+}
