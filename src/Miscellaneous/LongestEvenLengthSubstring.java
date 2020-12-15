6bacd6d26d118f9e5d88f652f72d057b97feea22
satyam <bitsat28@gmail.com>
2020-12-16 04:27:09 +0530

    --optimal Longest Even SubString

--- src/Miscellaneous/LongestEvenLengthSubstring.java
+++ src/Miscellaneous/LongestEvenLengthSubstring.java
@@ -2,45 +2,93 @@ package Miscellaneous;
 
 public class LongestEvenLengthSubstring {
 
-    static int findLength(String str)
+    // Time: 0(n3)
+    public static int findLength(String str)
     {
-        int n = str.length();
-        int maxlen = 0; // Initialize result
-
-        // Choose starting point of every
-        // substring
-        for (int i = 0; i < n; i++)
+        int len = str.length();
+        int maxlen = 0;
+        for (int i = 0; i < len; i++)
         {
-            // Choose ending point of even
-            // length substring
-            for (int j = i + 1; j < n; j += 2)
+            for (int j = i + 1; j < len; j += 2)
             {
-                // Find length of current substr
-                int length = j - i + 1;
-
-                // Calculate left & right sums
-                // for current substr
+                int lengthofSubString = j - i + 1;
                 int leftsum = 0, rightsum = 0;
-                for (int k = 0; k < length/2; k++)
+                for (int k = 0; k < lengthofSubString/2; k++)
                 {
                     leftsum += (str.charAt(i + k) - '0');
-                    rightsum += (str.charAt(i + k + length/2) - '0');
+                    rightsum += (str.charAt(i + k + lengthofSubString/2) - '0');
                 }
-
-                // Update result if needed
-                if (leftsum == rightsum && maxlen < length)
-                    maxlen = length;
+                if (leftsum == rightsum && maxlen < lengthofSubString)
+                    maxlen = lengthofSubString;
             }
         }
         return maxlen;
     }
 
-    // Driver program to test above function
+    //Time : 0(n2) space : 0(n2)
+    public static int findLengthDynamic(String str)
+    {
+        int n = str.length();
+        int maxlen = 0;
+
+        int sum[][] = new int[n][n];
+
+        // Fill the diagonal values for
+        // substrings of length 1
+        for (int i = 0; i < n; i++)
+            sum[i][i] = str.charAt(i) - '0';
+
+        // Fill entries for substrings of length 2 to n
+        for (int len = 2; len <= n; len++)
+        {
+            for (int i = 0; i < n - len + 1; i++)
+            {
+                int j = i + len - 1;
+                int k = len/2;
+
+                sum[i][j] = sum[i][j-k] + sum[j-k+1][j];
+
+                // Update result if 'len' is even, left and right sums are same and len is more than maxlen
+                if (len % 2 == 0 && sum[i][j-k] == sum[(j-k+1)][j] && len > maxlen)
+                    maxlen = len;
+            }
+        }
+        return maxlen;
+    }
+
+    //Time : 0(n2) space : 0(1)
+    public static int findLengthBest(String str, int n) {
+        int ans = 0;
+
+        // Consider all possible midpoints one by one
+        for (int i = 0; i <= n - 2; i++) {
+            /* For current midpoint 'i', keep expanding substring on
+           both sides, if sum of both sides becomes equal update ans */
+            int l = i, r = i + 1;
+            int lsum = 0, rsum = 0;
+
+            /* move on both sides till indexes go out of bounds */
+            while (r < n && l >= 0) {
+                lsum += str.charAt(l) - '0';
+                rsum += str.charAt(r) - '0';
+                if (lsum == rsum) {
+                    ans = Math.max(ans, r - l + 1);
+                }
+                l--;
+                r++;
+            }
+        }
+        return ans;
+    }
+
+
+
     public static void main(String[] args)
     {
         String str = "1538023";
-        System.out.println("Length of the substring is "
-                + findLength(str));
+        System.out.println("Length of the substring by using brute force is  " + findLength(str));
+        System.out.println("Length of the substring by using dynamic programming is " + findLengthDynamic(str));
+        System.out.println("Length of the substring by using Best is " + findLengthBest(str, str.length()));
     }
 
 }
