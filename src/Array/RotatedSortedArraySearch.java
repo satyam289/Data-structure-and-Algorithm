509fe6ca4b459b4001506fae3d0bb65a8be435e7
satyam289 <bitsat28@gmail.com>
2021-04-03 11:49:08 +0530

    --indentaion

--- src/Array/RotatedSortedArraySearch.java
+++ src/Array/RotatedSortedArraySearch.java
@@ -3,7 +3,7 @@ package Array;
 public class RotatedSortedArraySearch {
 
 	public static void main(String[] args) {
-		int[] a = { 4, 5, 1, 2, 3};
+		int[] a = { 4, 5, 1, 2, 3 };
 		int searchItem = 4;
 
 		int rotationPts = findPivot(a, 0, a.length - 1);
@@ -12,21 +12,21 @@ public class RotatedSortedArraySearch {
 		} else {
 			System.out.println("Rotated at index " + rotationPts + " The min value is " + a[rotationPts]);
 			if (a[0] < searchItem) // left part
-				System.out.println("searched item at index " + binarySearch(a, 0, rotationPts-1, searchItem));
-			else  // right part
+				System.out.println("searched item at index " + binarySearch(a, 0, rotationPts - 1, searchItem));
+			else // right part
 				System.out.println("searched item at index " + binarySearch(a, rotationPts, a.length - 1, searchItem));
 		}
 	}
 
 	public static int binarySearch(int[] arr, int low, int high, int key) {
 		if (high < low)
-            return -1;
-        int mid = (low + high) / 2;
-        if (key == arr[mid])
-            return mid;
-        if (key > arr[mid])
-            return binarySearch(arr, mid+1, high, key);
-        return binarySearch(arr, low, mid-1, key);
+			return -1;
+		int mid = (low + high) / 2;
+		if (key == arr[mid])
+			return mid;
+		if (key > arr[mid])
+			return binarySearch(arr, mid + 1, high, key);
+		return binarySearch(arr, low, mid - 1, key);
 	}
 
 	private static int findPivot(int[] arr, int low, int high) {
@@ -37,13 +37,13 @@ public class RotatedSortedArraySearch {
 			return low;
 
 		int mid = (low + high) / 2;
-		if (mid < high && arr[mid] > arr[mid+1])
-            return mid+1;  // returning min value
-        if (mid > low && arr[mid] < arr[mid-1])
-            return mid;  // returning min value
-			
-        if (arr[low] >= arr[mid])
-            return findPivot(arr, low, mid - 1);
-        return findPivot(arr, mid + 1, high);
+		if (mid < high && arr[mid] > arr[mid + 1])
+			return mid + 1; // returning min value
+		if (mid > low && arr[mid] < arr[mid - 1])
+			return mid; // returning min value
+
+		if (arr[low] >= arr[mid])
+			return findPivot(arr, low, mid - 1);
+		return findPivot(arr, mid + 1, high);
 	}
 }
