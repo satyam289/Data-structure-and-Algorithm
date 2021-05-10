9284ad6ca1cef89b43fca63d676ebfce7f5e1e63
satyam289 <bitsat28@gmail.com>
2021-05-11 00:49:21 +0530

    -- K closet point from origin

---
+++ src/Array/KClosetPointFromOrigin.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/k-closest-points-to-origin/
+public class KClosetPointFromOrigin {
+    
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> b[0] * b[0] + b[1] * b[1] - (a[0] * a[0] + a[1] * a[1]));
+
+        for (int[] point : points) {
+            maxHeap.add(point);
+            if (maxHeap.size() > k) {
+                maxHeap.remove();
+            }
+        }
+        int[][] result = new int[k][2];
+        while (k-- < 0) {
+            result[k] = maxHeap.remove();
+        }
+        return result;
+    }
+}
