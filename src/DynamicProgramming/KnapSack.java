c1111c9b97efe238585c3b053a4dc1054eb5da3a
satyam289 <bitsat28@gmail.com>
2021-07-09 22:53:47 +0530

    --knapsack topDown Recursion

--- src/DynamicProgramming/KnapSack.java
+++ src/DynamicProgramming/KnapSack.java
@@ -12,7 +12,7 @@ public class KnapSack {
                 if (j < wt[i - 1]) {
                     table[i][j] = table[i - 1][j];
                 } else {
-                    table[i][j] = Math.max(table[i - 1][j], table[i][j - wt[i - 1]] + val[i - 1]);
+                    table[i][j] = Math.max(table[i - 1][j], table[i - 1][j - wt[i - 1]] + val[i - 1]);
                 }
             }
         }
@@ -24,5 +24,25 @@ public class KnapSack {
         int[] val = { 1, 4, 5, 7 };
         int sum = 7;
         System.out.print(knapSackDP(wt, val, sum));
+    }
+
+    public int solve(int[] wt, int[] val, int sum) {
+        int[][] dp = new int[wt.length + 1][sum + 1];
+        return knapTopToDownApproach(dp, wt, val, sum, 0);
+    }
+
+    public int knapTopToDownApproach(int[][] dp, int[] wt, int[] val, int maxCapacity, int currentIndex) {
+        if (maxCapacity <= 0 || currentIndex >= wt.length)
+            return 0;
+
+        if (dp[currentIndex][maxCapacity] != 0)
+            return dp[currentIndex][maxCapacity];
+
+        int profit = 0;
+        if (wt[currentIndex] <= maxCapacity)
+            profit = Math.max(knapTopToDownApproach(dp, val, wt, maxCapacity - wt[currentIndex], currentIndex + 1)
+                    + val[currentIndex], knapTopToDownApproach(dp, val, wt, maxCapacity, currentIndex + 1));
+        dp[currentIndex][maxCapacity] = profit;
+        return dp[currentIndex][maxCapacity];
     }
 }
