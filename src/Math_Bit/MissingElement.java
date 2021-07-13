3795189826e4b765b245ef2fa7a4149988c46d88
satyam289 <42042570+satyam289@users.noreply.github.com>
2021-07-14 00:00:26 +0530

    First Missing Number

--- src/Math_Bit/MissingElement.java
+++ src/Math_Bit/MissingElement.java
@@ -28,4 +28,24 @@ public class MissingElement {
         }
         return x1 ^ x2;
     }
+  
+   //https://www.interviewbit.com/problems/first-missing-integer/
+   public int firstMissingPositive(ArrayList<Integer> A) {
+        for (int i = 0; i < A.size(); i++) {
+            int num = A.get(i);
+            int pos = num - 1;
+            
+            if (pos >= 0 && pos < A.size() && A.get(pos) != num) {
+                A.set(i, A.get(pos));
+                A.set(pos, num);
+                i--;
+            }
+        }
+        
+        for (int i = 0; i < A.size(); i++)
+            if (A.get(i) != i + 1)
+                return i + 1;
+        
+        return A.size() + 1;
+    }
 }
