e919f8a08f98081025f6b66c19dc5fda80058eb7
satyam289 <bitsat28@gmail.com>
2024-10-15 22:34:03 +0530

    top k frequency number

---
+++ src/leetcode/medium/TopKFrequentElements.java
@@ -0,0 +1,45 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class TopKFrequentElements {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        List<Integer> values = getTopKElements(nums, k);
+        System.out.println(values.toString());
+    }
+
+    public static List<Integer> getTopKElements(int[] nums, int  k) {
+        
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num : nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
+        }
+        @SuppressWarnings("unchecked")
+        ArrayList<Integer> [] list = new ArrayList[nums.length + 1];
+
+        for(Map.Entry<Integer, Integer> map : freqMap.entrySet()){
+            int frequency = map.getValue();
+            if(list[frequency] == null){
+                list[frequency] = new ArrayList<>();
+            }
+            list[frequency].add(map.getKey());
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i=list.length-1; i>=0; i--){
+            if(list[i]== null){
+                continue;
+            }
+            for(int j=0; j<list[i].size() && res.size() < k; j++){
+                res.add(list[i].get(j));
+            }
+        }
+        return res;
+    }
+}
