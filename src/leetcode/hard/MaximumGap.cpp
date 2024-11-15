7a86719742c3f5c92d1c893192dc37f6d7e1e806
satyam289 <bitsat28@gmail.com>
2024-11-15 21:34:54 +0530

    Optimised Maximum Gap

--- src/leetcode/hard/MaximumGap.cpp
+++ src/leetcode/hard/MaximumGap.cpp
@@ -37,4 +37,58 @@ public:
         }
         return maxGap;
     }
+
+    int maximumGap(vector<int>& nums)
+    {
+        if (num.empty() || nums.size() < 2)
+            return 0
+
+                   int maxVal = *max_element(nums.begin(), nums.end());
+        int exp = 1;
+        int radix = 10;
+        vector<int> aux(nums.size());
+
+        while (maxVal / exp > 0)
+        {
+            vector<int> count(radix, 0);
+
+            for (int i = 0; i < nums.size(); i++)
+            {
+                count[(num[i] / exp) % 10]++;
+            }
+            for (int i = 1; i < count.size; i++)
+            {
+                count[i] += count[i - 1];
+            }
+            for (int i = nums.size() - 1; i >= 0; i--)
+            {
+                aux[--count[(nums[i] / exp) % 10] = nums[i];
+            }
+            for (int i = 0; i < nums.size(); i++)
+            {
+                nums[i] = aux[i];
+            }
+            exp *= 10;
+        }
+        int maxGap = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            maxGap = max(nums[i + 1] - nums[i], maxGap);
+        }
+        return maxGap;
+    }
+
+    int maximumGap(vector<int>& nums)
+    {
+        if (nums.empty() || nums.size() < 2)
+            return 0;
+
+        sort(nums.begin(), nums.end());
+        int maxGap = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            maxGap = max(maxGap, nums[i + 1] - nums[i]);
+        }
+        return maxGap;
+    }
 };
